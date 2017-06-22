fd6d1cc3841a5d9d41d7b3d16057c09602aef6a4
imper <me@imper.info>
2017-06-22 15:50:22 +0200

    first commit

---
+++ src/Imper69/Core/DOMDocument.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright: IMPER.INFO Adrian Szuszkiewicz
+ * Date: 22.06.17
+ * Time: 15:48
+ */
+
+namespace Imper69\Core;
+
+
+class DOMDocument extends \DOMDocument
+{
+    public function createElementCData(string $name, string $value = null)
+    {
+        $element = $this->createElement($name);
+        $element->appendChild($this->createCDATASection($value));
+        return $element;
+    }
+}
---
+++ src/Imper69/Core/DateTime.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright: IMPER.INFO Adrian Szuszkiewicz
+ * Date: 22.06.17
+ * Time: 15:47
+ */
+
+namespace Imper69\Core;
+
+
+class DateTime extends \DateTime
+{
+    const KNOWN_FORMATS = [
+        'Y-m-d',
+        'd-m-Y',
+    ];
+
+    public function formatPL(string $format = 'Y-m-d H:i:s')
+    {
+        $oldFormat = $format;
+
+        foreach (self::KNOWN_FORMATS as $plFormat) $format = str_replace($plFormat, '', $format);
+
+        $suffix = (!empty($format) ? $this->format($format) : null);
+
+        $yesterday = new DateTime('yesterday');
+        $today = new DateTime('today');
+
+        if ($this->format('Ymd') === $yesterday->format('Ymd')) return 'wczoraj' . $suffix;
+        if ($this->format('Ymd') === $today->format('Ymd')) return 'dzisiaj' . $suffix;
+
+        return $this->format($oldFormat);
+    }
+
+    public function format($format = 'Y-m-d')
+    {
+        return parent::format($format);
+    }
+}
