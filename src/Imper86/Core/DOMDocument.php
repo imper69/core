5ac5a210fe02291dcf3ca5f6e59e1e728fc4b697
Adrian Szuszkiewicz <me@imper.info>
2018-04-06 14:41:27 +0200

    just some improvements into DOMDocument class

--- src/Imper86/Core/DOMDocument.php
+++ src/Imper86/Core/DOMDocument.php
@@ -10,10 +10,26 @@ namespace Imper86\Core;
 
 class DOMDocument extends \DOMDocument
 {
+    public function __construct($version = '1.0', $encoding = 'UTF-8')
+    {
+        parent::__construct($version, $encoding);
+    }
+
     public function createElementCData(string $name, string $value = null)
     {
         $element = $this->createElement($name);
         $element->appendChild($this->createCDATASection($value));
+        return $element;
+    }
+
+    public function createElement($name, $value = null, array $attributes = [])
+    {
+        $element = parent::createElement($name, $value);
+
+        foreach ($attributes as $key => $value) {
+            $element->setAttribute($key, $value);
+        }
+
         return $element;
     }
 }
